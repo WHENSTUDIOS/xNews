21fd988f4d0a8400e2b85c16d2c89a83bbe36db5
whenofficial <fsmitchm@gmail.com>
2018-03-01 16:03:30 +0800

    Create config.php

---
+++ src/lib/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//Configuration
+
+$config['siteName'] = '';
+$config['company'] = '';
+
+//Master User Information
+
+$config['user'] = 'admin';
+$config['email'] = 'admin@'.$config['siteName'].'.com';
+$config['pass'] = 'password';
+
+//Database Configuration
+$config['dbHost'] = 'localhost';
+$config['dbUser'] = 'root';
+$config['dbPass'] = '';
+$config['dbName'] = 'mynews';
+
+?>
