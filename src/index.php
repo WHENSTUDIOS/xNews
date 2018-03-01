ac3f1566245029dee4c60d5d4b6ca0d2d18a572a
whenofficial <fsmitchm@gmail.com>
2018-03-01 15:57:37 +0800

    Create router

--- src/index.php
+++ src/index.php
@@ -1,5 +1,9 @@
 <?php
 
+require('lib/db/mysqli.php');
+require('lib/config.php');
+
+//Get the router and display on page
 require('route/router.php');
 
 ?>
---
+++ src/view/article.php
@@ -0,0 +1,15 @@
+<?php
+
+// Main Article View for myNews
+
+$title = 'Article';
+
+//Get the headers
+require("view/header.php");
+
+?>
+<!-- Article View -->
+<?php
+//Get the footer
+require("view/footer.php");
+?>
