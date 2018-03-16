43c13ce10d72e352e1366a9c2ebb7f29e6b9ee81
whenofficial <fsmitchm@gmail.com>
2018-03-17 03:50:58 +0400

    Fix config

--- src/route/master.php
+++ src/route/master.php
@@ -1,13 +1,12 @@
 <?php
 
 function article($article, $theme){
-    require('lib/include/requires.php');
-    require('lib/include/includes.php');
     require('lib/config.php');
     require("view/$theme/article.php");
 }
 
 function content($content, $theme){
+    require('lib/config.php');
     require("view/$theme/$content.php");
 }
 
