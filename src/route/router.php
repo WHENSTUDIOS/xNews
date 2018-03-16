ba3e827ce11c6cf9a64e6db6194ff088bf539631
whenofficial <fsmitchm@gmail.com>
2018-03-16 17:16:48 +0400

    Fix pass function

--- src/route/router.php
+++ src/route/router.php
@@ -3,12 +3,13 @@
 //Main router
 $theme = $config['theme'];
 
+
 if(isset($_GET['article'])){
     article($_GET['article'], $theme);
 } elseif(isset($_GET['content'])){
     content($_GET['content'], $theme);
 } else {
-    content('index');
+    content('home', $theme);
 }
 
 
