f3a28d60342289a7d94b158004287953e0a2ba37
whenofficial <fsmitchm@gmail.com>
2018-03-16 17:15:20 +0400

    Pass $theme into get functions

--- src/route/master.php
+++ src/route/master.php
@@ -1,10 +1,10 @@
 <?php
 
-function article($article){
+function article($article, $theme){
     require("views/$theme/article.php");
 }
 
-function content($content){
+function content($content, $theme){
     require("views/$theme/$content.php");
 }
 
--- src/route/router.php
+++ src/route/router.php
@@ -4,9 +4,9 @@
 $theme = $config['theme'];
 
 if(isset($_GET['article'])){
-    article($_GET['article']);
+    article($_GET['article'], $theme);
 } elseif(isset($_GET['content'])){
-    content($_GET['content']);
+    content($_GET['content'], $theme);
 } else {
     content('index');
 }
