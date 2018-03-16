a309049329a44e46c3d6ce3b1b5c287bfabf1881
whenofficial <fsmitchm@gmail.com>
2018-03-16 17:46:18 +0400

    Add header.php

---
+++ src/view/Hydrogen/header.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+<!DOCTYPE html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php if(isset($title)){ echo $title; } else { echo 'xNews'; } ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+    <meta name="author" content="FREEHTML5.CO" />
+    
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Google Webfonts -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="styles/Hydrogen/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="styles/Hydrogen/icomoon.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="styles/Hydrogen/magnific-popup.css">
+	<!-- Salvattore -->
+	<link rel="stylesheet" href="styles/Hydrogen/salvattore.css">
+	<!-- Theme Style -->
+	<link rel="stylesheet" href="styles/Hydrogen/style.css">
+	<!-- Modernizr js/Hydrogen -->
+	<script src="js/Hydrogen/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/Hydrogen/respond.min.js/Hydrogen"></script>
+	<![endif]-->
+
+	</head>
+	<body>
--- src/view/Hydrogen/index.php
+++ src/view/Hydrogen/index.php
@@ -1,69 +1,11 @@
 
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js/Hydrogen lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js/Hydrogen lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js/Hydrogen lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js/Hydrogen"> <!--<![endif]-->
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Hydrogen &mdash; A free HTML5 Template by FREEHTML5.CO</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
+<?php
 
-  <!-- 
-	//////////////////////////////////////////////////////
+$title = 'Home';
 
-	FREE HTML5 TEMPLATE 
-	DESIGNED & DEVELOPED by FREEHTML5.CO
-		
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
+require('view/Hydrogen/header.php');
 
-	//////////////////////////////////////////////////////
-	 -->
-
-  <!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
-
-	<!-- Google Webfonts -->
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="styles/Hydrogen/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="styles/Hydrogen/icomoon.css">
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="styles/Hydrogen/magnific-popup.css">
-	<!-- Salvattore -->
-	<link rel="stylesheet" href="styles/Hydrogen/salvattore.css">
-	<!-- Theme Style -->
-	<link rel="stylesheet" href="styles/Hydrogen/style.css">
-	<!-- Modernizr js/Hydrogen -->
-	<script src="js/Hydrogen/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/Hydrogen/respond.min.js/Hydrogen"></script>
-	<![endif]-->
-
-	</head>
-	<body>
+?>
 		
 	<div id="fh5co-offcanvass">
 		<a href="#" class="fh5co-offcanvass-close js/Hydrogen-fh5co-offcanvass-close">Menu <i class="icon-cross"></i> </a>
