251d8d14845f31acdc116fa7ea5e5ae3d9237a25
whenofficial <fsmitchm@gmail.com>
2018-03-16 17:47:02 +0400

    Remove not required branding

--- src/view/Hydrogen/header.php
+++ src/view/Hydrogen/header.php
@@ -7,9 +7,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title><?php if(isset($title)){ echo $title; } else { echo 'xNews'; } ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-    <meta name="author" content="FREEHTML5.CO" />
     
 	<meta property="og:title" content=""/>
 	<meta property="og:image" content=""/>
@@ -21,10 +18,8 @@
 	<meta name="twitter:url" content="" />
 	<meta name="twitter:card" content="" />
 
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 	<link rel="shortcut icon" href="favicon.ico">
 
-	<!-- Google Webfonts -->
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	
@@ -40,10 +35,6 @@
 	<link rel="stylesheet" href="styles/Hydrogen/style.css">
 	<!-- Modernizr js/Hydrogen -->
 	<script src="js/Hydrogen/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/Hydrogen/respond.min.js/Hydrogen"></script>
-	<![endif]-->
 
 	</head>
 	<body>
