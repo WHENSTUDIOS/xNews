13500afc3de6807e6264e3d5bb0facd4ea711aec
whenofficial <fsmitchm@gmail.com>
2018-03-16 17:52:28 +0400

    Init footer

---
+++ src/view/Hydrogen/footer.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+	<footer id="fh5co-footer">
+		
+		<div class="container">
+			<div class="row row-padded">
+				<div class="col-md-12 text-center">
+					<p class="fh5co-social-icons">
+						<a href="#"><i class="icon-twitter"></i></a>
+						<a href="#"><i class="icon-facebook"></i></a>
+						<a href="#"><i class="icon-instagram"></i></a>
+						<a href="#"><i class="icon-dribbble"></i></a>
+						<a href="#"><i class="icon-youtube"></i></a>
+					</p>
+					<p><small>&copy; xNews <?php echo date('Y'); ?>. All rights reserved. <br>Designed by: <a href="http://freehtml5.co/" target="_blank">Hydrogen</a> | Wanna join in the fun? <a href="header.php" target="_blank">Register</a> </small></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- jQuery -->
+	<script src="js/Hydrogen/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/Hydrogen/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/Hydrogen/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/Hydrogen/jquery.waypoints.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/Hydrogen/jquery.magnific-popup.min.js"></script>
+	<!-- Salvattore -->
+	<script src="js/Hydrogen/salvattore.min.js"></script>
+	<!-- Main js/Hydrogen -->
+	<script src="js/Hydrogen/main.js"></script>
+
+	
+
+	
+	</body>
+</html>
