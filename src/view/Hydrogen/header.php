9c590f1a5e94fe8f08d9e26c998b7e5a3fe138d9
whenofficial <fsmitchm@gmail.com>
2018-03-17 03:58:13 +0400

    Added dynamic title

--- src/view/Hydrogen/header.php
+++ src/view/Hydrogen/header.php
@@ -38,3 +38,33 @@
 
 	</head>
 	<body>
+			
+	<div id="fh5co-offcanvass">
+		<a href="#" class="fh5co-offcanvass-close js/Hydrogen-fh5co-offcanvass-close">Menu <i class="icon-cross"></i> </a>
+		<h1 class="fh5co-logo"><a class="navbar-brand" href="index.html">Hydrogen</a></h1>
+		<ul>
+			<li class="active"><a href="index.html">Home</a></li>
+			<li><a href="about.html">About</a></li>
+			<li><a href="pricing.html">Pricing</a></li>
+			<li><a href="contact.html">Contact</a></li>
+		</ul>
+		<h3 class="fh5co-lead">Connect with us</h3>
+		<p class="fh5co-social-icons">
+			<a href="#"><i class="icon-twitter"></i></a>
+			<a href="#"><i class="icon-facebook"></i></a>
+			<a href="#"><i class="icon-instagram"></i></a>
+			<a href="#"><i class="icon-dribbble"></i></a>
+			<a href="#"><i class="icon-youtube"></i></a>
+		</p>
+	</div>
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<a href="#" class="fh5co-menu-btn js/Hydrogen-fh5co-menu-btn">Menu <i class="icon-menu"></i></a>
+					<?php if(basename('index.php')){ a($config['siteName'], '', 'navbar-brand', ''); } else { a($config['siteName'], 'index.php?content=index', 'navbar-brand', ''); } ?>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- END .header -->
--- src/view/Hydrogen/index.php
+++ src/view/Hydrogen/index.php
@@ -6,36 +6,6 @@ $title = 'Home';
 require('view/Hydrogen/header.php');
 
 ?>
-		
-	<div id="fh5co-offcanvass">
-		<a href="#" class="fh5co-offcanvass-close js/Hydrogen-fh5co-offcanvass-close">Menu <i class="icon-cross"></i> </a>
-		<h1 class="fh5co-logo"><a class="navbar-brand" href="index.html">Hydrogen</a></h1>
-		<ul>
-			<li class="active"><a href="index.html">Home</a></li>
-			<li><a href="about.html">About</a></li>
-			<li><a href="pricing.html">Pricing</a></li>
-			<li><a href="contact.html">Contact</a></li>
-		</ul>
-		<h3 class="fh5co-lead">Connect with us</h3>
-		<p class="fh5co-social-icons">
-			<a href="#"><i class="icon-twitter"></i></a>
-			<a href="#"><i class="icon-facebook"></i></a>
-			<a href="#"><i class="icon-instagram"></i></a>
-			<a href="#"><i class="icon-dribbble"></i></a>
-			<a href="#"><i class="icon-youtube"></i></a>
-		</p>
-	</div>
-	<header id="fh5co-header" role="banner">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<a href="#" class="fh5co-menu-btn js/Hydrogen-fh5co-menu-btn">Menu <i class="icon-menu"></i></a>
-					<a class="navbar-brand" href="index.html">Hydrogen</a>		
-				</div>
-			</div>
-		</div>
-	</header>
-	<!-- END .header -->
 	
 	
 	<div id="fh5co-main">
