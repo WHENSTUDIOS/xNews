7099c39ce40096ce207ce742cd6df8cc2117ba0a
whenofficial <fsmitchm@gmail.com>
2018-03-17 10:43:21 +0400

    Fix pdo typo

--- src/lib/db/pdo.php
+++ src/lib/db/pdo.php
@@ -2,7 +2,7 @@
 
 $host = $config['dbHost'];
 $db = $config['dbName'];
-$user = $config['[dbUser'];
+$user = $config['dbUser'];
 $pass = $config['dbPass'];
 
 $conn = new PDO("mysql:host=$host;dbname=$db", "$user", "$pass") or die;
