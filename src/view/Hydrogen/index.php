cbefb1c42cb64ed26f790bdc948df3b882c6c9ec
whenofficial <fsmitchm@gmail.com>
2018-03-17 05:01:39 +0400

    Sort images

--- src/view/Hydrogen/index.php
+++ src/view/Hydrogen/index.php
@@ -17,182 +17,182 @@ require('view/Hydrogen/header.php');
 
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_1.jpg" class="image-popup fh5co-board-img" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, eos?"><img src="view/Hydrogen/images/img_1.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_1.jpg" class="image-popup fh5co-board-img" title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, eos?"><img src="assets/images/img_1.jpg" alt="Free HTML5 Bootstrap template"></a>
         		</div>
         		<div class="fh5co-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, eos?</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_2.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_2.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_2.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_2.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Veniam voluptatum voluptas tempora debitis harum totam vitae hic quos.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_3.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_3.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_3.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_3.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Optio commodi quod vitae, vel, officiis similique quaerat odit dicta.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_4.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_4.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_4.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_4.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Dolore itaque deserunt sit, at exercitationem delectus, consequuntur quaerat sapiente.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_5.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_5.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_5.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_5.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Tempora distinctio inventore, nisi excepturi pariatur tempore sit quasi animi.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_6.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_6.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_6.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_6.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Sequi, eaque suscipit accusamus. Necessitatibus libero, unde a nesciunt repellendus!</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_7.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_7.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_7.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_7.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Necessitatibus distinctio eos ipsam cum hic temporibus assumenda deleniti, soluta.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_8.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_8.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_8.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_8.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Debitis voluptatum est error nulla voluptate eum maiores animi quasi?</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_9.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_9.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_9.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_9.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Maxime qui eius quisquam quidem quos unde consectetur accusamus adipisci!</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_21.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_21.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_21.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_21.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Deleniti aliquid, accusantium, consectetur harum eligendi vitae quaerat reiciendis sit?</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_10.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_10.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_10.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_10.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Incidunt, eaque et. Et odio excepturi, eveniet facilis explicabo assumenda.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_11.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_11.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_11.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_11.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Laborum dolores nihil voluptates quas alias distinctio fugiat tempora sit.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_12.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_12.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_12.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_12.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Sit, quis nulla amet numquam fugit, in reiciendis laboriosam dolor.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_13.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_13.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_13.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_13.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Possimus explicabo voluptatem natus nisi similique ipsa repudiandae? Quibusdam, fuga.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_14.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_14.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_14.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_14.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Magni repellendus iusto mollitia, quibusdam facilis incidunt. Sunt, repellat, voluptatem.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_15.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_15.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_15.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_15.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Unde iure rerum cupiditate explicabo quam aut vel earum numquam.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_16.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_16.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_16.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_16.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Qui nisi error dolorum dolor delectus, alias doloremque perspiciatis nemo.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_18.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_18.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_18.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_18.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Neque porro vero cumque natus nam voluptatibus, ratione, commodi labore.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_17.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_17.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_17.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_17.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Quisquam quia totam, sit ea maxime sint sed excepturi quod.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_19.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_19.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_19.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_19.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Nesciunt non iste ex nemo sapiente eum, provident nam corporis.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_20.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_20.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_20.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_20.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Harum repellat labore est cum ipsa, nesciunt neque mollitia adipisci?</div>
         		</div>
         	</div>
         	
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_22.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_22.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_22.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_22.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Quos repellendus repudiandae debitis reprehenderit cupiditate cumque accusamus exercitationem, harum.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_23.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_23.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_23.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_23.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Sunt numquam itaque delectus, dignissimos dolorem obcaecati vel, atque eos.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_24.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_24.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_24.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_24.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Adipisci consequuntur ipsa fugit perspiciatis eligendi. Omnis blanditiis, totam placeat.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_25.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_25.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_25.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_25.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Quos repellendus repudiandae debitis reprehenderit cupiditate cumque accusamus exercitationem, harum.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_26.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_26.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_26.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_26.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Sunt numquam itaque delectus, dignissimos dolorem obcaecati vel, atque eos.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_27.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_27.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_27.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_27.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Adipisci consequuntur ipsa fugit perspiciatis eligendi. Omnis blanditiis, totam placeat.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_28.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_28.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_28.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_28.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Adipisci consequuntur ipsa fugit perspiciatis eligendi. Omnis blanditiis, totam placeat.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_29.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_29.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_29.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_29.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Adipisci consequuntur ipsa fugit perspiciatis eligendi. Omnis blanditiis, totam placeat.</div>
         		</div>
         	</div>
         	<div class="item">
         		<div class="animate-box">
-	        		<a href="view/Hydrogen/images/img_30.jpg" class="image-popup fh5co-board-img"><img src="view/Hydrogen/images/img_30.jpg" alt="Free HTML5 Bootstrap template"></a>
+	        		<a href="assets/images/img_30.jpg" class="image-popup fh5co-board-img"><img src="assets/images/img_30.jpg" alt="Free HTML5 Bootstrap template"></a>
 	        		<div class="fh5co-desc">Adipisci consequuntur ipsa fugit perspiciatis eligendi. Omnis blanditiis, totam placeat.</div>
         		</div>
         	</div>
