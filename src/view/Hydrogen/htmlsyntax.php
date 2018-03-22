788486479ba7188d005b876b502a608c275a8225
whenofficial <fsmitchm@gmail.com>
2018-03-22 16:02:47 +0400

    Add syntax sheet

--- src/view/Hydrogen/htmlsyntax.php
+++ src/view/Hydrogen/htmlsyntax.php
@@ -3,45 +3,47 @@ $title = 'HTML Cheat Sheet';
 $name = 'htmlsyntax';
 $login = true;
 require('view/Hydrogen/header.php');
+
+function _htmlt($code, $desc, $usage){
+    echo "<tr><td><code>&lt;$code&gt;</code></td><td>$desc</td><td><code>&lt;$code&gt;$usage&lt;/$code&gt;</code></td></tr>";
+}
 ?>
 
-	<div id="fh5co-main">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					<h2><?php echo $title; ?></h2>
-					<?php
-					if(isset($_SESSION['success'])){
-						_success($_SESSION['success']);
-						unset($_SESSION['success']);
-					}
-					if(isset($_SESSION['error'])){
-						_error($_SESSION['error']);
-						unset($_SESSION['error']);
-					}
-					?>
-					<div class="fh5co-spacer fh5co-spacer-sm"></div>
-					<form action="lib/handlers/login.php" method="post">
-						<div class="row">
-						<div class="col-xl-4">
-                            Supports most <strong>HTML</strong> syntax. Take a look at our <?php a('Cheat Sheet', 'index.php?action=htmlsyntax', '', ''); ?>!
-                        <textarea id="editor" class="form-control" name="editor">Article body</textarea>
-                        <br>
-							<div class="col-xl-4">
-								<div class="form-group">
-                                    <input type="submit" class="btn btn-primary" value="Post">
-                                    <?php a('Save Draft', 'index.php?action=savedraft', 'btn btn-warning', ''); ?>
-								</div>
-							</div>
-						</div>
-					</form>
-					
-				</div>
-        		
-        	</div>
-       </div>
-	</div>
+    <div id="fh5co-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2>
+                        <?php echo $title; ?>
+                    </h2>
+                    <div class="fh5co-spacer fh5co-spacer-sm"></div>
+                    <form action="lib/handlers/login.php" method="post">
+                        <div class="row">
+                            <div class="col-xl-4">
+                                The article text editor supports the following HTML codes:
+                                <br>
+                                <br>
+                                <div class="col-xl-4">
+                                    <table style="width:100%">
+                                        <tr>
+                                            <th>Code</th>
+                                            <th>Description</th>
+                                            <th>Usage</th>
+                                        </tr>
+                                        <?php 
+                                            _htmlt('b', 'Makes the text bold', 'This text is bold!');
+                                        ?>
+                                    </table>
+                                </div>
+                            </div>
+                    </form>
 
-<?php
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <?php
 require('view/Hydrogen/footer.php');
 ?>
