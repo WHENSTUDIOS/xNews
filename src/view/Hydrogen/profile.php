605d7965f57feabf972166bc63e588e6fd244e4d
whenofficial <fsmitchm@gmail.com>
2018-03-23 14:35:08 +0400

    Init bio

--- src/view/Hydrogen/profile.php
+++ src/view/Hydrogen/profile.php
@@ -4,9 +4,15 @@ $title = 'Author Profile';
 $name = 'profile';
 $login = false;
 
-
+if(!isset($_GET['user'])){
+	route('c', 'index');
+}
 
 require 'view/Hydrogen/header.php';
+
+
+//GET USER LIST
+$profileGet = $_GET['user'];
 
 ?>
 
@@ -15,11 +21,29 @@ require 'view/Hydrogen/header.php';
 	<div id="fh5co-main">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					<h2>whenofficial</h2>
-					<div class="fh5co-spacer fh5co-spacer-sm"></div>
-					<p>
-					<p>No description given.</p>					
+				<div class="col-md-8 col-md-offset-2">	
+					<?php
+						$getuserlist = "SELECT * FROM users WHERE username = '$profileGet';";
+						if($query = $mysqli->query($getuserlist)){
+							while($rows = $query->fetch_assoc()){
+								if($rows['bio'] !== ''){
+									echo "
+									<h2>".$rows['username']."</h2>
+									<div class='fh5co-spacer fh5co-spacer-sm'></div>
+									<p>
+									<p>No description given</p>
+								";
+								} else {
+									echo "
+									<h2>".$rows['username']."</h2>
+									<div class='fh5co-spacer fh5co-spacer-sm'></div>
+									<p>
+									<p>".$rows['bio']."</p>
+								";
+								}
+							}
+						}
+					?>
 				</div>
 				
         		
