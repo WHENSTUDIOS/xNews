40e9a349c74360124c30b098bad75e5c6dd460a3
whenofficial <fsmitchm@gmail.com>
2018-03-23 12:35:51 +0800

    Change to callback

--- src/view/Hydrogen/theme.php
+++ src/view/Hydrogen/theme.php
@@ -1,15 +1,15 @@
 <?php
 
 $theme = $_GET['theme'];
-$return = $_GET['return'];
+$return = $_GET['callback'];
 $name = 'theme';
 
 if($theme === 'light'){
     setcookie('themeMode', 'light', 2147483647);
-    header("Location: index.php?content=$return");
+    route('c', $return);
 } elseif($theme === 'dark') {
     setcookie('themeMode', 'dark', 2147483647);
-    header("Location: index.php?content=$return");
+    route('c', $return);
 }
 
 ?>
