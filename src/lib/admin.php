7bddd03e0c1d6a4805e67a87ed20545f569a6337
whenofficial <fsmitchm@gmail.com>
2018-03-23 14:55:16 +0400

    Remove whitespace

--- src/lib/admin.php
+++ src/lib/admin.php
@@ -1,14 +1,12 @@
 <?php
 
-if(isset($_SESSION['username'])){
-    if($config['admin'] === $_SESSION['username']){
+if (isset($_SESSION['username'])) {
+    if ($config['admin'] === $_SESSION['username']) {
         $tempusername = $_SESSION['username'];
         $query = $mysqli->query("UPDATE users SET `level` = '4' WHERE username = '$tempusername';");
-        if($query){
+        if ($query) {
         }
         unset($tempusername);
         unset($query);
     }
 }
-
-?>
