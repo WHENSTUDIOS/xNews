96deeb2e8ce3cebdc3d1a52a331893dc1f1fa70f
whenofficial <fsmitchm@gmail.com>
2018-03-23 13:58:32 +0400

    Beautify

--- src/lib/requests.php
+++ src/lib/requests.php
@@ -1,20 +1,20 @@
 <?php
 //BEGIN REQUEST
-$u = $_SESSION['username'];
-
-/*
-User elevation request:
-Gets the defined user elevation level (i.e editor, user, administrator, etc).
-*/
-if ($u !== null) {
-    $query = $mysqli->query("SELECT * FROM users WHERE username = '$u';");
-    if ($query) {
-        while ($elev_rows = $query->fetch_assoc()) {
-			$_SESSION['level'] = $elev_rows['level'];
+if (isset($_SESSION['username'])) {
+    $u = $_SESSION['username'];
+    /*
+    User elevation request:
+    Gets the defined user elevation level (i.e editor, user, administrator, etc).
+     */
+    if ($u !== null) {
+        $query = $mysqli->query("SELECT * FROM users WHERE username = '$u';");
+        if ($query) {
+            while ($elev_rows = $query->fetch_assoc()) {
+                $_SESSION['level'] = $elev_rows['level'];
+            }
+        } else {
+            _error('Failed to get user elevation');
+            die;
         }
-    } else {
-        _error('Failed to get user elevation');
-        die;
     }
 }
-?>
