0048288b239aacb56bb72b1099363216a4046d6c
whenofficial <fsmitchm@gmail.com>
2018-03-26 16:45:25 +0800

    Add verify action

--- src/lib/actions/verify.php
+++ src/lib/actions/verify.php
@@ -2,4 +2,17 @@
 //Verification email
 $token = $_GET['token'];
 
+//Check if token actually exists
+if($query = $mysqli->query("SELECT * FROM `users` WHERE token = $token;"){
+    unset($query);
+    if($query = $mysqli->query("UPDATE users SET token = $new_token WHERE token = $token;")){
+        route('c', 'login');
+    } else {
+        _perror('Could not verify user.');
+    }
+} else {
+    //Token does not exist
+    _perror('Invalid verification token.');
+}
+
 ?>
