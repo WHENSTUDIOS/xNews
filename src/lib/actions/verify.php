fdd7c99c74ca81a83d75ac99072a9706cad56c75
whenofficial <fsmitchm@gmail.com>
2018-03-26 16:46:36 +0800

    Fix $new_token

--- src/lib/actions/verify.php
+++ src/lib/actions/verify.php
@@ -5,6 +5,7 @@ $token = $_GET['token'];
 //Check if token actually exists
 if($query = $mysqli->query("SELECT * FROM `users` WHERE token = $token;"){
     unset($query);
+    $new_token = $token . '-verified';
     if($query = $mysqli->query("UPDATE users SET token = $new_token WHERE token = $token;")){
         route('c', 'login');
     } else {
