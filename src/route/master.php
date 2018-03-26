739ced45a0b458e08bf1c4e7b66febdffb5f8ea6
whenofficial <fsmitchm@gmail.com>
2018-03-26 16:01:08 +0400

    Fix handler typo

--- src/route/master.php
+++ src/route/master.php
@@ -16,8 +16,8 @@ function content($content, $theme, $u, $mysqli){
 }
 
 function action($action){
-    if(file_exists("lib/handler/$action.php")){
-        require("lib/handler/$action.php");
+    if(file_exists("lib/handlers/$action.php")){
+        require("lib/handlers/$action.php");
     } else {
         echo 'Action does not exist';
     }
