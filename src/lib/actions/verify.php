f72268a3e7485a10741ef8412efff3698e8b5536
whenofficial <fsmitchm@gmail.com>
2018-03-26 14:38:05 +0400

    Fix if() typo

--- src/lib/actions/verify.php
+++ src/lib/actions/verify.php
@@ -3,7 +3,7 @@
 $token = $_GET['token'];
 
 //Check if token actually exists
-if($query = $mysqli->query("SELECT * FROM `users` WHERE token = $token;"){
+if($query = $mysqli->query("SELECT * FROM `users` WHERE token = $token;")){
     unset($query);
     $new_token = $token . '-verified';
     if($query = $mysqli->query("UPDATE users SET token = $new_token WHERE token = $token;")){
