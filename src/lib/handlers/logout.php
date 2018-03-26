16331d4d5617ba78ec333ac0e7af82590dc98edc
whenofficial <fsmitchm@gmail.com>
2018-03-26 16:06:55 +0400

    Merge to pdo

--- src/lib/handlers/logout.php
+++ src/lib/handlers/logout.php
@@ -1,5 +1,4 @@
 <?php
-require 'lib/functions.php';
 session_start();
 session_destroy();
 route('c', 'index');
--- src/lib/handlers/verify.php
+++ src/lib/handlers/verify.php
@@ -2,9 +2,11 @@
 //Verification email
 $token = $_GET['token'];
 
+$verify_query = $conn->prepare("SELECT * FROM `users` WHERE token = :token;");
+
 //Check if token actually exists
-if($query = $mysqli->query("SELECT * FROM `users` WHERE token = $token;")){
-    unset($query);
+if($verify_query->execute(array(':token' => $token,))){
+    unset($verify_query);
     $new_token = $token . '-verified';
     if($query = $mysqli->query("UPDATE users SET token = $new_token, `enabled` = '1' WHERE token = $token;")){
         route('c', 'login');
