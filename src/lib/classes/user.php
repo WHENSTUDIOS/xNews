f654146a8bc8ae45c65b1128443000cd93e6c648
whenofficial <fsmitchm@gmail.com>
2018-03-27 20:19:07 +0800

    Create user class

---
+++ src/lib/classes/user.php
@@ -0,0 +1,16 @@
+<?php
+
+class UserData {
+    private $username;
+    private $email;
+
+    function getUsername(){
+        return $this->username;
+    }
+
+    function getEmail(){
+        return $this->email;
+    }
+}
+
+?>
