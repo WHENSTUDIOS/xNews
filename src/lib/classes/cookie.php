f7aed4f834c9fb733d174b51c7a3a200f629caed
whenofficial <fsmitchm@gmail.com>
2018-03-27 21:30:21 +0800

    Init cookie class

---
+++ src/lib/classes/cookie.php
@@ -0,0 +1,17 @@
+<?php
+
+class GetCookie {
+
+    public function name($cookie){
+
+    }
+    public function content($cookie){
+
+    }
+    public function expiry($cookie){
+        
+    }
+
+}
+
+?>
