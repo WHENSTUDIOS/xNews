af95d6c6c0691a813f4c765bb18e6ce32d237d41
whenofficial <fsmitchm@gmail.com>
2018-04-01 14:02:06 +0400

    Add level check to edit functions

--- src/xnews_lv/resources/views/pages/posts/post.blade.php
+++ src/xnews_lv/resources/views/pages/posts/post.blade.php
@@ -8,18 +8,20 @@
                     {!!$post->body!!}
                     <hr>
                     <h3>
-                        <small>
+                                <form action="../posts/{{$post->id}}" method="POST">
+                                <small>
                             Posted by
                             <strong>
-                                <form action="../posts/{{$post->id}}" method="POST">
                                 @csrf
                                 <a href="../profile/{{$post->user['id']}}">{{$post->user['name']}} </a></strong>
                                 | Last modified at <strong>{{$post->updated_at}}</strong>
-                                @if(Auth::check())
+                                @if(Auth::check() && Auth::user()->level >=2)
                                 <span class="functions"><a href="../posts/{{$post->id}}/edit">Edit</a>
+                                @if(Auth::check() && Auth::user()->level >=3)
                                 |
                                 <input name="_method" type="hidden" value="DELETE">
                                 <input type="submit" class="delete-button error" value="Delete"/></span>
+                                @endif
                                 @endif
                                 </form>
                         </small>
