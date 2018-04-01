8588ff8455824b77a7ec3aad5efab0b291bb39d5
whenofficial <fsmitchm@gmail.com>
2018-04-01 12:11:45 +0400

    Add functions

--- src/xnews_lv/resources/views/pages/posts/post.blade.php
+++ src/xnews_lv/resources/views/pages/posts/post.blade.php
@@ -4,8 +4,20 @@
         <div class="row">
             <div class="item">
                 <div class="fh5co-desc">
-                    <h1>{{$post->title}} <small>Posted by <strong><a href="profile/{{$post->user['id']}}">{{$post->user['name']}}</a></strong></small></h1>
-                    {{$post->body}}
+                    <h1>{{$post->title}}</h1>
+                    {!!$post->body!!}
+                    <hr>
+                    <h3>
+                        <small>
+                            Posted by
+                            <strong>
+                                <a href="profile/{{$post->user['id']}}">{{$post->user['name']}}</a>
+                            </strong>
+                                <span class="functions"><a href="posts/{{$post->id}}/edit">Edit</a>
+                                |
+                                <a href="posts/{{$post->id}}/delete">Delete </span>
+                        </small>
+                        </h2>
                 </div>
             </div>
         </div>
