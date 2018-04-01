02c4eb53e38b5abea62a549c732e8f01630d29bd
whenofficial <fsmitchm@gmail.com>
2018-04-01 15:40:51 +0400

    Start work on dashboard

--- src/xnews_lv/app/Http/Controllers/DashboardController.php
+++ src/xnews_lv/app/Http/Controllers/DashboardController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class DashboardController extends Controller
 {
     public function dashboard(){
+        if(Auth::check()){
         return view('dashboard.dashboard');
+        } else {
+        return redirect('login');
+        }
     }
 }
--- src/xnews_lv/resources/views/dashboard/dashboard/header.blade.php
+++ src/xnews_lv/resources/views/dashboard/dashboard/header.blade.php
@@ -38,9 +38,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>G</b>fi</span>
+      <span class="logo-mini"><b>x</b>N</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Gamer</b>fi</span>
+      <span class="logo-lg"><b>x</b>News Blog</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -97,7 +97,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="{{ asset('img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">{{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -105,8 +105,8 @@
                 <img src="{{ asset('img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  {{Auth::user()->name}}
+                  <small>{{Auth::user()->email}}</small>
                 </p>
               </li>
               <!-- Menu Body -->
@@ -127,10 +127,10 @@
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <a href="{{url('profile/'.Auth::user()->id)}}" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="{{url('logout')}}" class="btn btn-default btn-flat">Log out</a>
                 </div>
               </li>
             </ul>
