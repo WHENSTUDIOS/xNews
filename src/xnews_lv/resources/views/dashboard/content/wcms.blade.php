a645d56edb8b1439fcca601027520fb5990c26fa
whenofficial <fsmitchm@gmail.com>
2018-04-03 13:45:57 +0400

    Fix content typo

--- src/xnews_lv/resources/views/dashboard/content/wcms.blade.php
+++ src/xnews_lv/resources/views/dashboard/content/wcms.blade.php
@@ -26,7 +26,7 @@
                 </div>
                 <!-- /.box-header -->
                 <!-- form start -->
-                <form method="POST" autocomplete="nope" action="{{url('dashboard/settings/wcms/general')}}">
+                <form method="POST" autocomplete="nope" action="{{url('dashboard/content/wcms/general')}}">
                     @csrf
                     <div class="box-body">
                         <div class="form-group">
