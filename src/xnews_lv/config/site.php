8ecff936995634e942e2132438aee9685673070f
whenofficial <fsmitchm@gmail.com>
2018-04-03 15:20:25 +0800

    Add allow switcher to site cfg

--- src/xnews_lv/config/site.php
+++ src/xnews_lv/config/site.php
@@ -4,7 +4,8 @@ return array(
     'data' => array(
         'name' => 'xNews Blog',
         'url' => 'http://localhost:8000',
-        'allow_username_change' => 'true'
+        'allow_username_change' => 'true',
+        'allow-switcher' => 'false'
     )
 )
 
