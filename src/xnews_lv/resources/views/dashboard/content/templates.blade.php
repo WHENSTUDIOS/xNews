a0d4a6068f861e83b4dc6c025fbdc7b22f4eadd5
whenofficial <fsmitchm@gmail.com>
2018-04-05 14:58:46 +0800

    typo

--- src/xnews_lv/resources/views/dashboard/content/templates.blade.php
+++ src/xnews_lv/resources/views/dashboard/content/templates.blade.php
@@ -38,8 +38,8 @@
                                         <form action="{{url('dashboard/settings/templates/active/'.$template->id)}}" method="POST">
                                         @csrf
                                         <input name="_method" type="hidden" value="PUT">
-                                        </form>
                                         <td><input type="submit" class="btn btn-primary btn-xs">Make Active</a> |
+                                        </form>
                                         <form action="{{url('dashboard/settings/templates/delete/'.$template->id)}}" method="POST">
                                         @csrf
                                         <input name="_method" type="hidden" value="DELETE"><a class="btn btn-success btn-xs" href="{{url('dashboard/settings/templates/edit/'.$template->id)}}">Edit</a> | <input type="submit" class="btn btn-danger btn-xs" value="Delete"/></span></td>
