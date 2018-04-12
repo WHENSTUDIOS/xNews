669cccc1e2434844fe49453a9e62915c089ed8e6
whenofficial <fsmitchm@gmail.com>
2018-04-12 09:29:17 +0800

    Check if socials are null

--- src/xnews_lv/resources/views/pages/profile.blade.php
+++ src/xnews_lv/resources/views/pages/profile.blade.php
@@ -8,10 +8,18 @@
         <div id="fh5co-board" data-columns>
             <center><h1>{{$user->name}}</h1>
             <p class="fh5co-social-icons">
+                        @if($social->twitter != null)
 						<a href="https://twitter.com/{{$social->twitter}}"><i class="icon-twitter"></i></a>
+                        @endif
+                        @if($social->facebook !=null)
 						<a href="{{$social->facebook}}"><i class="icon-facebook"></i></a>
+                        @endif
+                        @if($social->youtube !=null)
 						<a href="https://youtube.com/c/{{$social->youtube}}"><i class="icon-youtube"></i></a>
+                        @endif
+                        @if($social->instagram != null)
                         <a href="https://instagram.com/{{$social->instagram}}"><i class="icon-instagram"></i></a>
+                        @endif
 					</p>
                 @if($social->bio == '')
                 <p>No description given.</p>
