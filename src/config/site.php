aba890999d017296f23e174b0158f9796928887e
whenofficial <fsmitchm@gmail.com>
2018-04-14 21:40:17 +0400

    Remove trash

--- src/config/site.php
+++ src/config/site.php
@@ -7,7 +7,11 @@ return array(
         'allow_username_change' => 'true',
         'allow-switcher' => 'false',
         'maintenance' => 'false',
-        'install' => 'false'
+        'install' => 'false',
+        'db_host' => '127.0.0.1',
+        'db_user' => 'root',
+        'db_pass' => '',
+        'db_name' => 'xnews-lv'
     )
 )
 
