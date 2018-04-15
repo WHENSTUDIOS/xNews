795ea3aefe4f0c523755a4fb97f18455d72d5a34
whenofficial <fsmitchm@gmail.com>
2018-04-15 14:57:35 +0400

    Style messages

--- src/resources/views/dashboard/dashboard/messages.blade.php
+++ src/resources/views/dashboard/dashboard/messages.blade.php
@@ -1,9 +1,11 @@
 @if(Session::get('success'))
                     <div class="alert alert-success">
+                    <strong>Success</strong>:&nbsp;
                     {{ Session::get('success') }}
                     </div>
                 @elseif(Session::get('error'))
                     <div class="alert alert-danger">
+                    <strong>Error</strong>:&nbsp;
                     {{ Session::get('error') }}
                     </div>
                 @endif
