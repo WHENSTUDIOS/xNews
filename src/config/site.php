295fa4bd3d150c1d63e81adee597825619d33f2c
whenofficial <fsmitchm@gmail.com>
2018-04-18 14:23:50 +0400

    tests

--- src/config/site.php
+++ src/config/site.php
@@ -12,7 +12,7 @@ return array(
         'db_user' => 'root',
         'db_pass' => '',
         'db_name' => 'xnews-lv',
-        'dashtheme' => 'default'
+        'dashtheme' => 'purple'
     )
 )
 
