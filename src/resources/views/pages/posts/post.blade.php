cbbf52e44cf19dcb7079f6792e423541634186ed
whenofficial <fsmitchm@gmail.com>
2018-04-18 22:00:05 +0400

    Create foundations

--- src/resources/views/pages/posts/post.blade.php
+++ src/resources/views/pages/posts/post.blade.php
@@ -4,7 +4,9 @@
         <div class="row">
             <div class="item">
                 <div class="fh5co-desc">
-                    <h1>{{$post->title}} <small>{{$post->categories['name']}}</small></h1>
+                    <h1>{{$post->title}}
+                        <small>{{$post->categories['name']}}</small>
+                    </h1>
                     {!!$post->body!!}
                     <hr>
                     <h3>
@@ -12,26 +14,31 @@
                             Posted by
                             <strong>
                                 @csrf
-                                <a href="../profile/{{$post->user['id']}}">{{$post->user['name']}}</a></strong> | <strong>{{$post->views}}</strong> view{{ $post->views == 1 ? '' : 's' }}
+                                <a href="../profile/{{$post->user['id']}}">{{$post->user['name']}}</a>
+                            </strong> |
+                            <strong>{{$post->views}}</strong> view{{ $post->views == 1 ? '' : 's' }}
                             </strong>
                             @if($post->created_at == $post->updated_at) {{$post->created_at->diffForHumans()}} @else | Last modified
                             <i>{{$post->updated_at->diffForHumans()}}</i>
-                            @endif @if(Auth::check() && Auth::user()->level >=2)
-                            @if($post->visible == 0)
-                            | This post is <strong>hidden</strong>. You can un-hide it from the <a href="{{url('dashboard/articles/edit/'.$post->id)}}">Dashboard</a>.
+                            @endif @if(Auth::check() && Auth::user()->level >=2) @if($post->visible == 0) | This post is
+                            <strong>hidden</strong>. You can un-hide it from the
+                            <a href="{{url('dashboard/articles/edit/'.$post->id)}}">Dashboard</a>. @endif
                             @endif
-                            <span class="functions">
-                                <a href="../posts/{{$post->id}}/edit">Edit</a>
-                                @if(Auth::check() && Auth::user()->level >=3) |
-                                <form action="../posts/{{$post->id}}" method="POST" style="display:inline;">
-                                @csrf
-                                    <input name="_method" type="hidden" value="DELETE">
-                                    <input type="submit" class="delete-button error" value="Delete" />
-                            </span>
-                            @endif @endif
                             </form>
                         </small>
                         </h2>
+                        <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>Comments</h4>
+                    <br>
+                    <p><a>whenofficial</a> | 2 weeks ago</p>
+                    <p>Great article!</p>
+                    <hr>
+                    <p><a>whenofficial</a> | 2 weeks ago</p>
+                    <p>Great article!</p>
                 </div>
             </div>
         </div>
