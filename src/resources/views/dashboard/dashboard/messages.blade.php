8d78d25fd264fefcf3eb2a853d168c8fb126f349
whenofficial <fsmitchm@gmail.com>
2018-04-18 13:52:17 +0400

    dismissable alerts

--- src/resources/views/dashboard/dashboard/messages.blade.php
+++ src/resources/views/dashboard/dashboard/messages.blade.php
@@ -1,10 +1,12 @@
 @if(Session::get('success'))
-                    <div class="alert alert-success">
+                    <div class="alert alert-success alert-dismissible">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                     <strong>Success</strong>:&nbsp;
                     {{ Session::get('success') }}
                     </div>
                 @elseif(Session::get('error'))
-                    <div class="alert alert-danger">
+                    <div class="alert alert-danger alert-dismissible">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                     <strong>Error</strong>:&nbsp;
                     {{ Session::get('error') }}
                     </div>
