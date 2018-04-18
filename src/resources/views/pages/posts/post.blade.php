84bd3155f151a44029c9cd6a6d0fcc049fb0dfd0
whenofficial <fsmitchm@gmail.com>
2018-04-18 14:05:28 +0400

    Show category on main index

--- src/resources/views/pages/posts/post.blade.php
+++ src/resources/views/pages/posts/post.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="item">
                 <div class="fh5co-desc">
-                    <h1>{{$post->title}} <small>Category template</small></h1>
+                    <h1>{{$post->title}} <small>{{$post->category}}</small></h1>
                     {!!$post->body!!}
                     <hr>
                     <h3>
