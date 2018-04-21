44a700c26526a644b3a5239a8d61f6f6bc6400ef
whenofficial <fsmitchm@gmail.com>
2018-04-21 16:43:37 +0400

    Optimized loading time

--- src/resources/views/dashboard/dashboard/header.blade.php
+++ src/resources/views/dashboard/dashboard/header.blade.php
@@ -18,16 +18,7 @@
 
 
   <link rel="stylesheet" href="{{ asset('css/skins/skin-'.Config::get('site.data.dashtheme').'.min.css') }}">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="bower_components/morris.js/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
