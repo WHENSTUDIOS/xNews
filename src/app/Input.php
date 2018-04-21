56cddb60d99baeef7c741e6df4d13881588b0e39
whenofficial <fsmitchm@gmail.com>
2018-04-21 15:40:56 +0400

    Fix typo

--- src/app/Input.php
+++ src/app/Input.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Input extends Model
 {
     public static function sanitize($input){
-        $sanitized = strip_tags($request->input('bio'));
+        $sanitized = strip_tags($input);
         return preg_replace('/(^|)@([\w_]+)/', '<a href="../profile/$2">@$2</a>', $sanitized);    
     }
 }
